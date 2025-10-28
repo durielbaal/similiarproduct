8250ff7cb56ce0eeb52087d35266caa7c5406023
durielbaal <michaelogwu1997@gmail.com>
2025-10-28 09:47:05 +0100

    feat: complete project initialization

---
+++ src/main/java/knowmad/company/similiarproduct/SimiliarproductApplication.java
@@ -0,0 +1,13 @@
+package knowmad.company.similiarproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiliarproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiliarproductApplication.class, args);
+	}
+
+}
